e54473d8daf0772e8952f2bfe0b9c80401ed617a
see-you <44763778@qq.com>
2022-03-23 14:41:21 +0800

    :recycle: first

---
+++ codeJava-demo1/src/main/java/com/hsx/test.java
@@ -0,0 +1,16 @@
+package com.hsx;
+
+/**
+ * Demo
+ *
+ * @author J.FLA
+ * @version 1.0
+ * @date 2022/03/23 14:37
+ * Description :
+ */
+public class test {
+    public static void main(String[] args) {
+        //测试
+        System.out.println("Hello world !");
+    }
+}
---
+++ codeJava-demo2/src/main/java/com/hsx/demo.java
@@ -0,0 +1,15 @@
+package com.hsx;
+
+/**
+ * Demo
+ *
+ * @author J.FLA
+ * @version 1.0
+ * @date 2022/03/23 14:39
+ * Description :
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hsx 第一次Hub上传项目");
+    }
+}
